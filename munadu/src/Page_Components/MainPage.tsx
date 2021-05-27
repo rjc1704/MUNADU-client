8bd587852c8cf446c495e4bf3d118efc668db15d
DaeseongKim <77534023+daeseongkim05@users.noreply.github.com>
2021-05-27 09:07:56 +0900

    :sparkles: [MainPage] 설문조사로 이동하는 고정 버튼 추가

--- munadu/src/Page_Components/MainPage.tsx
+++ munadu/src/Page_Components/MainPage.tsx
@@ -1,9 +1,9 @@
-import React, { ReactNode, useState } from "react";
+import { useState } from "react";
 import styled from "styled-components";
-
 import Banner from "../Function_Components/MainPage/Banner";
 import MartialListForm from "../Function_Components/MainPage/MartialListForm";
 import RecommendForm from "../Function_Components/MainPage/RecommendForm";
+import { useHistory } from "react-router";
 
 export default function MainPage() {
   const [isSelected, setIsSelected] = useState(true);
@@ -16,10 +16,39 @@ export default function MainPage() {
     height: 100vh;
     overflow: auto;
   `;
+
+  const SurveyBtn = styled.div`
+    position: fixed;
+    right: 16%;
+    bottom: 5%;
+    width: 5rem;
+    height: 5rem;
+    border-radius: 50%;
+    background-color: #1c1c1c;
+    text-align: center;
+    line-height: 5rem;
+    color: white;
+    font-family: Noto Sans KR Medium;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    filter: drop-shadow(0px 0px 8px rgba(0, 0, 0, 0.25));
+    cursor: pointer;
+    &:hover {
+      background-color: rgba(12, 12, 12, 0.61);
+    }
+  `;
+  const history = useHistory();
+  const moveToSurveyPage = () => {
+    history.push({
+      pathname: "/surveypage",
+    });
+  };
   return (
     <Main>
       <Banner isSelected={isSelected} checkSelected={checkSelected}></Banner>
       {isSelected ? <RecommendForm /> : <MartialListForm />}
+      <SurveyBtn onClick={() => moveToSurveyPage()}>설문조사</SurveyBtn>
     </Main>
   );
 }
